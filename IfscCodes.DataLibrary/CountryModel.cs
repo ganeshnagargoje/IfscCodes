4d9e00a5aebf890ac2093efeffda655090566349
ganeshnagargoje <ganesh.nagargoje@gmaol.com>
2015-06-13 17:51:46 +0530

    First Checkin
    First Checkin

---
+++ IfscCodes.DataLibrary/CountryModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfscCodes.DataLibrary
+{
+   public class CountryModel
+    {
+       public int Create(Country item)
+       {
+           using (IfscEntities context = new IfscEntities())
+           {
+               context.Countries.Add(item);
+               context.SaveChanges();
+               return item.CountryId;
+           }
+       }
+
+       public int Update(Country item)
+       {
+           using (IfscEntities context = new IfscEntities())
+           {
+               Country itemToUpdate = GetCountry(item.CountryId);
+               itemToUpdate.CountryName = item.CountryName;
+               itemToUpdate.CountryCode = item.CountryCode;
+               itemToUpdate.Active = item.Active;
+               context.SaveChanges();
+               return item.CountryId;
+           }
+       }
+       public List<Country> GetCountries()
+       {
+           using (IfscEntities context = new IfscEntities())
+           {
+               List<Country> list = context.Countries.Where(c => c.Active == true).ToList<Country>();
+               return list;
+           }
+       }
+
+       public Country GetCountry(int Id)
+       {
+           using (IfscEntities context = new IfscEntities())
+           {
+               Country item = context.Countries.Where(c => c.Active == true && c.CountryId == Id).FirstOrDefault();
+               return item;
+           }
+       }
+    }
+}
---
+++ IfscCodes.Web/Controllers/CountryController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IfscCodes.DataLibrary;
+namespace IfscCodes.Web.Controllers
+{
+    public class CountryController : Controller
+    {
+        //
+        // GET: /Country/
+
+        public ActionResult Index()
+        {
+            List<Country> countries = new CountryModel().GetCountries();
+            return View(countries);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
